d72b3c2583de86c49f6e208905c8bb7a63fec51d
너나들이 <tychejin@naver.com>
2023-05-24 20:09:48 +0900

    Chore: MQTT + RabbitMQ

--- mqtt-rabbitmq/src/main/java/com/example/mqttrabbitmq/config/MqttConfig.java
+++ mqtt-rabbitmq/src/main/java/com/example/mqttrabbitmq/config/MqttConfig.java
@@ -6,12 +6,19 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class MqttConfig {
 
   private static final String SERVER_URI = "tcp://127.0.0.1:1883";
-  private static final String CLIENT_ID = "client_id";
+  private static final String CLIENT_ID = "mqttClient";
   private static final String USER_NAME = "guest";
   private static final String PASSWORD = "guest";
 
@@ -30,5 +37,21 @@ public class MqttConfig {
     iMqttClient.connect(mqttConnectOptions);
 
     return iMqttClient;
+  }
+
+  @Bean
+  public MessageChannel mqttInputChannel() {
+    return new DirectChannel();
+  }
+
+  @Component
+  public static class MqttSubscriber {
+
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public void handleMqttMessage(Message<String> message, @Header(MqttHeaders.TOPIC) String topic) {
+      String payload = message.getPayload();
+      System.out.println("Received MQTT message on topic " + topic + ": " + payload);
+      // MQTT 메시지 처리 로직 구현
+    }
   }
 }
--- mqtt-rabbitmq/src/test/java/com/example/mqttrabbitmq/config/MqttConfigTest.java
+++ mqtt-rabbitmq/src/test/java/com/example/mqttrabbitmq/config/MqttConfigTest.java
@@ -2,10 +2,25 @@ package com.example.mqttrabbitmq.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@Slf4j
-@RunWith(SpringRunner.class)
-@ActiveProfiles(inheritProfiles = false, resolver = ActiveProfileResolver.class)
-@SpringBootTest(classes = CoreApplication.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+@SpringBootTest
 class MqttConfigTest {
 
+  @Autowired
+  private MqttConfig.MqttSubscriber mqttSubscriber;
+
+  @Test
+  public void testHandleMqttMessage() {
+
+    String topic = "T0001";
+    String payload = "Test Message";
+
+    Message<String> message = new GenericMessage<>(payload);
+    mqttSubscriber.handleMqttMessage(message, topic);
+  }
 }
