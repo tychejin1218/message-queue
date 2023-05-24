f97e8143fd62e8b7608089c3b759d7479ff6bafc
너나들이 <tychejin@naver.com>
2023-05-24 17:21:56 +0900

    Feat: MQTT + RabbitMQ

---
+++ mqtt-rabbitmq/src/main/java/com/example/mqttrabbitmq/MqttRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqttrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttRabbitmqApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MqttRabbitmqApplication.class, args);
+  }
+
+}
---
+++ mqtt-rabbitmq/src/test/java/com/example/mqttrabbitmq/MqttRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttRabbitmqApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
