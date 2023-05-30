145ef60056bd049515b2b6d1e9930a1a978a3c30
너나들이 <tychejin@naver.com>
2023-05-30 17:42:35 +0900

    Chore: Stomp + RabbitMQ

--- stomp-rabbitmq/src/main/java/com/example/stomprabbitmq/contoller/MessageContoller.java
+++ stomp-rabbitmq/src/main/java/com/example/stomprabbitmq/contoller/MessageContoller.java
@@ -1,14 +1,10 @@
 package com.example.stomprabbitmq.contoller;
 
 import com.example.stomprabbitmq.dto.MessageDto;
-import java.util.Map;
+import com.example.stomprabbitmq.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageContoller {
 
-  private final SimpMessagingTemplate simpMessagingTemplate;
-  private final RabbitAdmin rabbitAdmin;
+  private final MessageService messageService;
 
   /**
    * 메시지 브로커로 메시지를 전송
@@ -30,54 +25,19 @@ public class MessageContoller {
    */
   @RequestMapping(value = "/send/message", method = RequestMethod.POST)
   public ResponseEntity<?> sendMessage(@RequestBody MessageDto messageDto) {
-    simpMessagingTemplate.convertAndSend("/queue/" + messageDto.getTeacherId(),
-        messageDto.getMessage());
+    messageService.sendMessage(messageDto);
     return ResponseEntity.ok().build();
-  }
-
-  @RequestMapping(value = "/send/message/header", method = RequestMethod.POST)
-  public ResponseEntity<?> sendMessageHeader(@RequestBody MessageDto messageDto) {
-
-    Map headers = Map.of(
-        "persistent", false,  // 메시지 브로커에 의해 메시지를 디스크에 지속적으로 저장 여부를 설정
-        "exclusive", false, // 생성한 연결에 대해 전용으로 접근 권한 여부를 설정
-        "auto-delete", true,  // 모든 구독이 해제될 때 자동으로 삭제되는지 여부를 설정
-        "x-expires", 1000 * 60 * 30, // 큐가 사용되지 않은 상태(Consumer가 없을 때) 유지되는 시간이며, 초과 시 자동으로 삭제
-        "x-message-ttl", 1000 * 60 * 10, // 큐(Queue)에 전송된 메시지가 유지되는 시간이며, 초과 시 자동으로 삭제(큐 단위로 설정)
-        "x-max-length", 100 // 큐(Queue)에 저장할 수 있는 메시지 개수를 설정
-    );
-
-    String queueName = messageDto.getTeacherId();
-    if (!isQueueExists(queueName)) {
-      createQueue(queueName, headers);
-    }
-
-    simpMessagingTemplate.convertAndSend("/queue/" + queueName, messageDto.getMessage(), headers);
-
-    return ResponseEntity.ok().build();
-  }
-
-  /***
-   * 큐(Queue) 존재 여부 확인
-   *
-   * @param queueName 큐(Queue)명
-   * @return 큐 존재 여부
-   */
-  public boolean isQueueExists(String queueName) {
-    return rabbitAdmin.getQueueProperties(queueName) != null;
   }
 
   /**
-   * 큐(Queue)를 생성
+   * 메시지 브로커로 메시지를 전송
    *
-   * @param queueName 설명
+   * @param messageDto 메시지 정보
+   * @return 메시지 전송 결과
    */
-  public void createQueue(String queueName, Map<String, Object> arguments) {
-    Queue queue = new Queue(queueName,
-        (Boolean) arguments.get("persistent"),
-        (Boolean) arguments.get("exclusive"),
-        (Boolean) arguments.get("auto-delete"),
-        arguments);
-    rabbitAdmin.declareQueue(queue);
+  @RequestMapping(value = "/send/message/header", method = RequestMethod.POST)
+  public ResponseEntity<?> sendMessageHeader(@RequestBody MessageDto messageDto) {
+    messageService.sendMessageHeader(messageDto);
+    return ResponseEntity.ok().build();
   }
 }
