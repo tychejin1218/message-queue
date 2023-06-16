386138a60a5cd453ddfc722069b54ee612bbd78c
너나들이 <tychejin@naver.com>
2023-06-16 11:01:41 +0900

    Chore: SpringBoot + ActiveMQ + STOMP 연동

--- activemq-stomp-sample/src/main/java/com/example/activemq/contoller/MessageController.java
+++ activemq-stomp-sample/src/main/java/com/example/activemq/contoller/MessageController.java
@@ -5,6 +5,7 @@ import com.example.activemq.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +24,7 @@ public class MessageController {
    * @param messageDto 발행할 메시지의 DTO 객체
    * @return ResponseEntity 객체로 응답을 반환
    */
+  @CrossOrigin
   @RequestMapping(value = "/send/message", method = RequestMethod.POST)
   public ResponseEntity<?> sendMessage(@RequestBody MessageDto messageDto) {
     messageService.sendMessage(messageDto);
