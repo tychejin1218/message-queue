a8c4aee2c38d5e406a8f8a57fc0cc4583afd362e
너나들이 <tychejin@naver.com>
2023-06-16 16:31:12 +0900

    Chore: SpringBoot + ActiveMQ + STOMP 연동

--- activemq-stomp-sample/src/main/java/com/example/activemq/config/WebSocketConfig.java
+++ activemq-stomp-sample/src/main/java/com/example/activemq/config/WebSocketConfig.java
@@ -4,23 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompReactorNettyCodec;
-import org.springframework.messaging.tcp.TcpOperations;
-import org.springframework.messaging.tcp.reactor.ReactorNettyTcpClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Slf4j
 @RequiredArgsConstructor
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker // WebSocket 메시지 브로커를 활성화
 @Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-  @Value("${spring.activemq.broker-url}")
-  private String activemqBrokerUrl;
 
   @Value("${spring.activemq.stomp.host}")
   private String activemqStompHost;
