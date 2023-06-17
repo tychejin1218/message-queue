74c4af5d2ec63d6da6fec057580ce58da3c39981
너나들이 <tychejin@naver.com>
2023-06-17 10:05:09 +0900

    Chore: SpringBoot + ActiveMQ + STOMP 연동

--- activemq-stomp-sample/src/main/java/com/example/activemq/config/WebSocketConfig.java
+++ activemq-stomp-sample/src/main/java/com/example/activemq/config/WebSocketConfig.java
@@ -32,7 +32,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
    */
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws") // Client에서 WebSocker 연결할 엔드포인트를 설정
+    registry.addEndpoint("/ws") // WebSocket 엔드포인트 설정
         .setAllowedOriginPatterns("*")
         .withSockJS(); // withSockJS()를 사용하여 SockJS를 활성화
   }
@@ -42,7 +42,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
    */
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableStompBrokerRelay("/queue", "/topic", "/exchange") // 메시지 구독(subscribe)할 경로를 설정
+    registry.enableStompBrokerRelay("/queue") // 메시지를 구독할 경로를 설정
         // ActiveMQ 브로커와 연결을 위한 호스트, 가상 호스트 및 포트, 관리자 로그인 설정
         .setRelayHost(activemqStompHost)
         .setRelayPort(Integer.parseInt(activemqStompPort))
